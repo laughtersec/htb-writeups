4a13c8bd384e211a83b88e0c108b028365736d43
laughtersec <178651847+laughtersec@users.noreply.github.com>
2025-08-16 11:04:02 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,36 +1,97 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "HackTheBox Writeups",
-    // Change to your desired site title
-    ...
+    pageTitle: "HackTheBox Write-ups.",
+    pageTitleSuffix: "",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "en-US",
     baseUrl: "laughtersec.github.io/htb-writeups",
-    // Change to your site URL without https://.
-    // This is your own domain,
-    // or "<github-user-name>.github.io/<repository-name>" when using GitHub Pages.
-    // See below for details
-    ...
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
-    // Change to tell Quartz what date to display on notes
-    // Valid options:
-    // "created", use when the note was created.
-    // "modified", use when the note was last modified.
-    // "published", use when the note was published.
-    // See Quartz docs for details.
-    ...
-    }
-  }
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
+    },
+  },
   plugins: {
     transformers: [
-      ...
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      // Sets how Quartz should resolve links between notes.
-      // Should match the settings you use in Obsidian.
-      // Valid options:
-      // "shortest"
-      // "relative"
-      // "absolute"
-      ...
-    ]
-    ...
-  }
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
+      Plugin.CustomOgImages(),
+    ],
+  },
 }
+
+export default config
